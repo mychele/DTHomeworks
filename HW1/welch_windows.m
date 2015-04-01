abc37a1b61487f57fcc744cc0846cee0589b1710
mychele <michele.polese@gmail.com>
2015-04-01 09:31:00 +0200

    Improved welch over windows

--- HW1/welch_windows.m
+++ HW1/welch_windows.m
@@ -10,16 +10,25 @@ z = z.z.'; % make a column vector
 z = z - mean(z); % remove average
 K = length(z); % signal length
 
-locs = 1:1000; % just to initialize for the first iteration
-peaks = zeros(1000,1); % just to initialize for the first iteration
 
 
-step = 13; %distance between the first two sample of each window
-span = 130; %actual size of the window
+step = 20; %distance between the first two sample of each window
+span = 100; %actual size of the window
 overlap = span - step;
-acc_locs = zeros(span, 1);
+
+locs_w = 1:1000;
+locs_c = 1:1000;
+locs_per = 1:1000; % just to initialize for the first iteration
+peaks = zeros(1000,1); % just to initialize for the first iteration
+peaks_w = zeros(1000,1);
+peaks_c = zeros(1000,1);
+acc_locs_per = zeros(span, 1);
+acc_locs_w = zeros(span, 1);
+acc_locs_c = zeros(span, 1);
 
 max_iter = floor((K-span)/step);
+
+window_span = kaiser(span, 5.65);
 
 figure
 for i = 0:max_iter
@@ -28,7 +37,7 @@ for i = 0:max_iter
     %% Compute different spectral analysis
     
     % PERIODOGRAM pg 84
-    Z = fft(z_part);
+    Z = fft(z_part.*window_span);
     periodogr = abs(Z).^2/span;
     
     % compute WELCH estimator pg 85
@@ -64,24 +73,44 @@ for i = 0:max_iter
 %     hold on
 %     plot(omega/(2*pi), 10*log10(sigma_w*abs(H).^2), 'Color', 'm', 'LineWidth', 1);
 %     hold on
-%     plot(locs/span, 10*log10(peaks),'o','MarkerSize',12)
+%     plot(locs_w/span, 10*log10(peaks_w),'o','MarkerSize',12)
 %     hold off
 %     axis([0, 1, 0, 50])
 %     legend('Welch', 'Correlogram', 'Periodogram', 'AR(3)')
 %     title(sprintf('Spectral analysis from sample %d', i*step))
+%     
+%     pause();
     
-    %pause();
-    
-    [peaks, locs] = findpeaks(P_welch);
+    [peaks, locs_per] = findpeaks(abs(periodogr));
+    [peaks_w, locs_w] = findpeaks(P_welch);
+    [peaks_c, locs_c] = findpeaks(abs(correlogram));
+
     % 
-    acc_locs(locs) = acc_locs(locs) + 1;
-    
+    acc_locs_per(locs_per) = acc_locs_per(locs_per) + 1;
+    acc_locs_w(locs_w) = acc_locs_w(locs_w) + 1;
+    acc_locs_c(locs_c) = acc_locs_c(locs_c) + 1;
     
     
 end
 
 % normalize
-acc_locs = acc_locs/i;
+acc_locs_per = acc_locs_per/i;
+acc_locs_w = acc_locs_w/i;
+acc_locs_c = acc_locs_c/i;
 
-bar((1:span)/span, acc_locs)
-axis([0, 1, 0, 0.4])
+
+figure
+subplot(1, 3, 1)
+bar((1:span)/span, acc_locs_per)
+axis([0, 1, 0, max(acc_locs_per)])
+title('periodogram peaks accumulation')
+subplot(1, 3, 2)
+bar((1:span)/span, acc_locs_w)
+axis([0, 1, 0, max(acc_locs_per)])
+title('welch')
+subplot(1, 3, 3)
+bar((1:span)/span, acc_locs_c)
+axis([0, 1, 0, max(acc_locs_per)])
+title('correlogram')
+
+disp(find(acc_locs_per > 0.7)/span)
