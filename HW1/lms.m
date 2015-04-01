c23496e44968271646efd0657d1c3822c8d0d675
DvdMgr <magrin.davide@gmail.com>
2015-04-01 14:57:11 +0200

    Added titles to plots

--- HW1/lms.m
+++ HW1/lms.m
@@ -24,6 +24,7 @@ N = 3;
 N = 3; % order of the predictor
 upper_limit = 999; %MATLAB requires indices from 1 to 401
 c = zeros(N, upper_limit + 1); % init c vector, no info -> set to 0
+%c(:,1) = -a +5;
 % each column of this matrix is c(k), a vector with coefficients from 1 to
 % N (since we are implementing the predictor)!
 e = zeros(1, upper_limit);
@@ -34,7 +35,7 @@ mu = 0.1/(autoc_z(1)*N); % actually mu must be > 0 and < 2/(N r_z(0))
 % watch out, in the predictor y(k) = transp(x(k-1))c(k)
 for k = 1:upper_limit
     if (k < N + 1)
-        z_k_1 = flipud([zeros(N - k + 1, 1); z(1:k - 1)]); % input vector z_vec_(k-1) of lenght N
+        z_k_1 = flipud([zeros(N - k + 1, 1); z(1:k - 1)]); % input vector z_vec_(k-1) of length N
         % for k = 1 z(1:0) is an empty matrix
         y_k = z_k_1.'*c(:, k);
     else
@@ -43,24 +44,32 @@ for k = 1:upper_limit
     end
     e_k = z(k) - y_k; % the reference signal d(k) is actually the input at sample k
     e(k) = e_k;
-    c(:, k + 1) = c(:, k) + mu*e_k*conj(z_k_1); % update the filter, c(k+1) = c(k) + mu*e(k)*conj(c(k-1))
+    c(:, k + 1) = c(:, k) + mu*e_k*conj(z_k_1); % update the filter, c(k+1) = c(k) + mu*e(k)*conj(z(k-1))
 end
 
 figure
 subplot(2, 1, 1)
 plot(1:upper_limit+1, real(c(1, :)), 1:upper_limit+1, -real(a(1)))
+title('Real part of c1');
 subplot(2, 1, 2)
 plot(1:upper_limit+1, imag(c(1, :)), 1:upper_limit+1, -imag(a(1)))
+title('Imaginary part of c1');
 
 figure
 subplot(2, 1, 1)
 plot(1:upper_limit+1, real(c(2, :)), 1:upper_limit+1, -real(a(2)))
+title('Real part of c2');
 subplot(2, 1, 2)
 plot(1:upper_limit+1, imag(c(2, :)), 1:upper_limit+1, -imag(a(2)))
+title('Imaginary part of c2');
 figure
 subplot(2, 1, 1)
 plot(1:upper_limit+1, real(c(3, :)), 1:upper_limit+1, -real(a(3)))
+title('Real part of c3');
 subplot(2, 1, 2)
 plot(1:upper_limit+1, imag(c(3, :)), 1:upper_limit+1, -imag(a(3)))
+title('Imaginary part of c3');
+
 figure, plot(1:upper_limit, 10*log10(abs(e).^2))
+title('Error function at each iteration');
 
