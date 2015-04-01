1fd2f04790527057a445db7e9e377736be8c0d74
DvdMgr <magrin.davide@gmail.com>
2015-04-01 14:55:56 +0200

    Moved plotting to a separate function

--- HW1/filter_wiener.m
+++ HW1/filter_wiener.m
@@ -4,8 +4,6 @@ clc;
 
 %% Load data
 z = load('data for hw1.mat');
-fir_bs_1 = load('fir_bs_1.mat');
-firbs = fir_bs_1.fir_bs_1;
 z = z.z.'; % make a column vector
 z = z - mean(z); % remove average
 K = length(z); % signal length
@@ -13,11 +11,12 @@ autoc = autocorrelation(z, length(z)/5);
 
 %% High pass filter
 
-z = filter(hp, 1, z);
+z = filter(hp30, 1, z);
 
+plot_spectrum(z);
 
 %% Filter pg 137 BC
-N = 36;
+N = 30;
 w0 = 2*pi*0.78; % 0.78 is probably our freq
 vec =  w0 * (0:N-1);
 E_w0 = exp(1i * vec).';
@@ -38,59 +37,22 @@ figure, plot(w/(2*pi), 10*log10(H))
 axis([0, 1, -30, 10])
 figure, plot(w/(2*pi), angle(H))
 
+% Filter using the Weiner filter
 spectra = filter(c_opt, 1, z);
 
-
-%% Compute different spectral analysis
-
-% PERIODOGRAM pg 84
-Z = fft(spectra);
-periodogr = abs(Z).^2/K;
-
-% compute WELCH estimator pg 85 
-D = 200; % window size
-window = kaiser(D, 5.65);
-S = D/2; %common samples
-P_welch = welchPsd(spectra, window, S);
-
-% CORRELOGRAM
-N_corr = ceil(K/5); % N_corr is the order of the autocorrelation estimate
-window_correlogram = kaiser(2*N_corr + 1, 5.65); % window centered around N_corr
-correlogram = correlogramPsd(spectra, window_correlogram, N_corr);
-
-% AR model: order estimation
-% compute variance of AR model and plot it to identify the knee
-% it's computed up to K/5 - 1, don't know if it makes sense
-autoc = autocorrelation(spectra, N_corr);
-upp_limit = 60;
-sigma_w = zeros(1, upp_limit);
-for N = 1:upp_limit
-    [~, sigma_w(N)] = arModel(N, autoc);
-end
-figure, plot(1:upp_limit, 10*log10(sigma_w))
-
-% the knee is apparently at N = 3
-% compute the vector of coefficients a
-N = 3;
-[a, sigma_w] = arModel(N, autoc);
-[H, omega] = freqz(1, [1; a], K, 'whole');
-
-
-clear a autoc D fir_bs_1 N N_corr S upp_limit window window_correlogram
-
-
-
-%% Plot PSD estimate
-
+% Plot original and filtered signal
 figure
-plot((1:K)/K, 10*log10(P_welch), 'Color', 'r', 'LineWidth', 2)
-hold on
-plot((1:K)/K, 10*log10(abs(correlogram)), 'Color', 'b', 'LineWidth', 1)
-hold on
-plot((1:K)/K, 10*log10(periodogr), 'c:')
-hold on
-plot(omega/(2*pi), 10*log10(sigma_w*abs(H).^2), 'Color', 'm', 'LineWidth', 1);
-hold off
-axis([0, 1, -40, 10])
-legend('Welch', 'Correlogram', 'Periodogram', 'AR(3)', 'Location', 'SouthEast')
-title('Spectral analysis')
+subplot(2,2,1)
+plot(real(z))
+title('Real part of original signal');
+subplot(2,2,3)
+plot(imag(z), 'r')
+title('Imaginary part of original signal');
+subplot(2,2,2)
+plot(real(spectra))
+title('Real part of spectral line');
+subplot(2,2,4)
+plot(imag(spectra), 'r')
+title('Imaginary part of spectral line');
+
+plot_spectrum(spectra);
