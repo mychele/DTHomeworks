280525c7506b6d7afd32e7cb45d30a4d1f03411c
mychele <michele.polese@gmail.com>
2015-04-08 15:12:04 +0200

    Minor details in autocorrelation_biased comments

--- HW1/autocorrelation_biased.m
+++ HW1/autocorrelation_biased.m
@@ -1,14 +1,12 @@
 function [ autoc ] = autocorrelation_biased( z1, N_corr )
-%AUTOCORRELATION Summary of this function goes here
-%   Detailed explanation goes here
+%AUTOCORRELATION biased estimator
 
 K = length(z1);
 autoc = zeros(N_corr + 1, 1);
-% we should use the unbiased estimator pg 82 1.478
 for n = 1:(N_corr + 1)
     d = z1(n:K);
     b = conj(z1(1:(K - n + 1)));
-    c = K; % check this scaling factor
+    c = K;
     autoc(n) = d.' * b / c;
 end
 
