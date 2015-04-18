c22c4f0d04b8968b7cdb11e13784b1365afc1db5
DvdMgr <magrin.davide@gmail.com>
2015-04-18 17:16:02 +0200

    A commit just for Michele. Enjoy.

--- HW2/channelModel.m
+++ HW2/channelModel.m
@@ -17,9 +17,6 @@ tau = 0:Tc:final_tau;
 tau_rms = 0.3*T;  % Tc = 1, as it is the fundamental sampling time
 pdp = 1/tau_rms * exp(-tau/tau_rms);
 
-% Plot the PDP
-figure, stem(tau,pdp), title('PDP'), xlabel('tau'), ylabel('E[|g_i|^2]'), hold on;
-
 C = sqrt(K*sum(pdp)/(K + 1));    % This is the deterministic component inside of g_1
 M_d = sum(pdp) - C^2;
 
@@ -30,4 +27,23 @@ M_d = sum(pdp) - C^2;
 pdp = pdp/sum(pdp);
 
 % Plot the normalised PDP
-figure, stem(tau,pdp), title('PDP'), xlabel('tau'), ylabel('E[|g_i|^2]');
+figure, stem(tau,10*log10(pdp)), title('PDP'), xlabel('tau'), ylabel('E[|g_i|^2]');
+
+%% Show the behavior of |g_i(nTc)| for n = 0:1999, dropping the transient
+% We now have to generate the time behavior of the i-th coefficient. For
+% this, we use the model in figure 4.36, page 315.
+% The transient will have length equal to the length of the convolution of
+% the filter cascade. We will drop that quantity of samples.
+% The book suggests using 1/10 < f_d*Tp < 1/5. For f_d*Tp = 1/7.5 we should
+% pick Tp = 26.66 T ~= 25 T
+Tq = Tc; % Fundamental sampling time. This is the same as Tc, right???
+Tp = 25 * Tq; % Sampling time used for filtering the white noise
+g_samples_needed = 2000;
+w_samples_needed = g_samples_needed / Tp;
+
+% Generate complex-valued Gaussian white noise with zero mean and unit
+% variance
+w = wgn(w_samples_needed,1,0,'complex');
+
+% Filter the wgn with a narrowband filter. The filter will have the
+% classical Doppler spectrum in the frequency domain, with f_d * T = 5*10^-3
