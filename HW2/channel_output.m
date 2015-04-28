113e6bcb5da563e67db7803edb5495253cd9bed9
andrea-dittadi <andrea.dittadi@gmail.com>
2015-04-28 09:29:38 +0200

    Changed g to h in channel_output

--- HW2/channel_output.m
+++ HW2/channel_output.m
@@ -1,20 +1,20 @@
-function [ d, h_mean ] = channel_output( x, T, Tc, sigma_w, N_h, g_mat )
+function [ d, h_mean ] = channel_output( x, T, Tc, sigma_w, N_h, h_mat )
 % CHANNEL_OUTPUT Generate channel output (that is the desired signal) via a
 % polyphase implementation (for Nh<=4).
 
 d = zeros(T*length(x),1);
-g_used_coeff = zeros(4, length(x)); % check these dimensions
+h_used_coeff = zeros(4, length(x)); % check these dimensions
 for k = 0:length(x)-1
     % Generate white noise
     w = wgn(4, 1, 10*log10(sigma_w), 'complex');
     for idx = 0:4-1 % actually idx varies between 0 and 4 since there are four branches
         if (idx < N_h)
-            d(k*T + idx*Tc + 1) = g_mat(idx+1,k*T + idx*Tc+1) * x(k+1) + w(idx+1);
+            d(k*T + idx*Tc + 1) = h_mat(idx+1,k*T + idx*Tc+1) * x(k+1) + w(idx+1);
             % store the coefficient actually used, it will be useful later on
-            g_used_coeff(idx + 1, k + 1) = g_mat(idx+1,k*T + idx*Tc+1);
+            h_used_coeff(idx + 1, k + 1) = h_mat(idx+1,k*T + idx*Tc+1);
         else
             d(k*T + idx*Tc + 1) = 0 + w(idx+1); % no ray, just the noise
-            g_used_coeff(idx + 1, k + 1) = 0;
+            h_used_coeff(idx + 1, k + 1) = 0;
         end
     end
 end
@@ -23,6 +23,6 @@ end
 
 % Compute the mean coefficient of impulse response of each ray over the interval of
 % interest of L samples in order to compare them with the estimated impulse response.
-h_mean = mean(g_used_coeff, 2);
+h_mean = mean(h_used_coeff, 2);
 
 end
