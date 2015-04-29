b261fc335d5be2c510664843208c27ac4775e95a
andrea-dittadi <andrea.dittadi@gmail.com>
2015-04-29 17:19:48 +0200

    First complete check of channel_output

--- HW2/channel_output.m
+++ HW2/channel_output.m
@@ -1,20 +1,22 @@
 function [ d, h_mean ] = channel_output( x, T, Tc, sigma_w, N_h, h_mat )
-% CHANNEL_OUTPUT Generate channel output (that is the desired signal) via a
-% polyphase implementation (for Nh<=4).
+% CHANNEL_OUTPUT Generates channel output (that is the desired signal) via a
+% polyphase implementation (for Nh<=4). Returns the channel output d given
+% the input parameters, and a vector with the average coefficients of the
+% actual impulse response during the considered window.
 
-d = zeros(T*length(x),1);
-h_used_coeff = zeros(4, length(x)); % check these dimensions
-for k = 0:length(x)-1
+d = zeros(T * length(x), 1);
+h_used_coeff = zeros(4, length(x));
+for k = 0 : length(x)-1
     % Generate white noise
     w = wgn(4, 1, 10*log10(sigma_w), 'complex');
-    for idx = 0:4-1 % actually idx varies between 0 and 4 since there are four branches
-        if (idx < N_h)
-            d(k*T + idx*Tc + 1) = h_mat(idx+1,k*T + idx*Tc+1) * x(k+1) + w(idx+1);
+    for i = 0:3 % Branch index
+        if (i < N_h)
+            d(k*T + i*Tc + 1) = h_mat(i+1,k*T + i*Tc+1) * x(k+1) + w(i+1);
             % store the coefficient actually used, it will be useful later on
-            h_used_coeff(idx + 1, k + 1) = h_mat(idx+1,k*T + idx*Tc+1);
+            h_used_coeff(i + 1, k + 1) = h_mat(i+1,k*T + i*Tc+1);
         else
-            d(k*T + idx*Tc + 1) = 0 + w(idx+1); % no ray, just the noise
-            h_used_coeff(idx + 1, k + 1) = 0;
+            d(k*T + i*Tc + 1) = w(i+1); % No ray, just the noise
+            h_used_coeff(i + 1, k + 1) = 0;
         end
     end
 end
