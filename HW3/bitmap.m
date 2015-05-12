f203347117d8d391622b971d2a3f55f2af427ad9
DvdMgr <magrin.davide@gmail.com>
2015-05-12 10:13:21 +0200

    Fixed bug

--- HW3/bitmap.m
+++ HW3/bitmap.m
@@ -10,13 +10,13 @@ function [output] = bitmap(input)
     
     % Map each couple of values to the corresponding symbol
     for idx = 1:2:L-1
-        if (isequal(input(idx:idx+1), [0 0] ))
+        if (isequal(input(idx:idx+1), [0; 0] ))
             output(idx) = -1-1i;
-        elseif (isequal(input(idx:idx+1), [0 1] ))
+        elseif (isequal(input(idx:idx+1), [0; 1] ))
             output(idx) = 1-1i;
-        elseif (isequal(input(idx:idx+1), [1 0] ))
+        elseif (isequal(input(idx:idx+1), [1; 0] ))
             output(idx) = -1+1i;
-        elseif (isequal(input(idx:idx+1), [1 1] ))
+        elseif (isequal(input(idx:idx+1), [1; 1] ))
             output(idx) = +1+1i;
         end
     end
