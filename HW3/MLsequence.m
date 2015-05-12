b6a96616d553dfa657a825b3da8661aa1d2d0be8
DvdMgr <magrin.davide@gmail.com>
2015-05-12 09:31:44 +0200

    Added script for problem 1, imported the MLsequence generator from HW2

---
+++ HW3/MLsequence.m
@@ -0,0 +1,26 @@
+function [ p ] = MLsequence( L )
+% Generate a Maximum Length Pseudo Noise sequence, using shift and xor
+% operators. L is the desired length of the resulting PN sequence. The
+% script can handle L = 3, 7, 15, 31, 63 and 127.
+
+% Extract r from the given L
+r = log2(L+1);
+p = zeros(L,1);
+p(1:r) = ones(1,r).'; % Set arbitrary initial condition
+for l = r+1:(L) % Skip the initial condition for the cycle
+    switch L
+        case 3
+            p(l) = xor(p(l-1), p(l-2));
+        case 7
+            p(l) = xor(p(l-2), p(l-3));
+        case 15
+            p(l) = xor(p(l-3), p(l-4));
+        case 31
+            p(l) = xor(p(l-3), p(l-5));
+        case 63
+            p(l) = xor(p(l-5), p(l-6));
+        case 127
+            p(l) = xor(p(l-6), p(l-7));
+    end
+end
+end
