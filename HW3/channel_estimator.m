d419bf41090a6df4be68315122d0d1520687b39f
mychele <michele.polese@gmail.com>
2015-05-13 09:19:24 +0200

    channel_estimator estimates t0, interpolates q_hat to get hi

--- HW3/channel_estimator.m
+++ HW3/channel_estimator.m
@@ -34,7 +34,7 @@ trainingsymbols = bitmap(trainingseq);
 
 snr = 20; %dB
 snr_lin = 10^(snr/10);
-[r, sigma_w] = channel_output(trainingsymbols, T, Tc, snr_lin);
+[r, sigma_w, q] = channel_output(trainingsymbols, T, Tc, snr_lin);
 
 %% Estimate qhat
 maxN = 44;
@@ -45,11 +45,51 @@ for N = 1:maxN % N is the supposed length of the impulse response of the channel
     % N_i is the number of coefficients of the filter of the i-th branch.
     N_i(1:4-n_short) = ceil(N/4);
     N_i(4-n_short + 1 : 4) = ceil(N/4) - 1;
-    [h_hat, d_hat] = h_estimation( trainingsymbols(end-(L+max(N_i)-1) + 1 : end), ...
+    [~, r_hat] = h_estimation( trainingsymbols(end-(L+max(N_i)-1) + 1 : end), ...
         r(end - 4*(L+max(N_i)-1) + 1: end), L, N_i);
-    d_no_trans = r(end-length(d_hat)+1 : end);
-    error_func(N) = sum(abs(d_hat - d_no_trans).^2)/length(d_hat); % TODO check if we should divide for L?
+    d_no_trans = r(end-length(r_hat)+1 : end);
+    error_func(N) = sum(abs(r_hat - d_no_trans).^2)/length(r_hat); % TODO check if we should divide for L?
 end
 
 figure
 plot(10*log10(error_func)), hold on, plot(10*log10(sigma_w*ones(1, maxN)))
+
+%% Plot q and qhat fot N = 28
+N = 28;
+% Compute the supposed length of each branch
+n_short = mod(4-N, 4); % Num branches with a shorter filter than others
+% N_i is the number of coefficients of the filter of the i-th branch.
+N_i(1:4-n_short) = ceil(N/4);
+N_i(4-n_short + 1 : 4) = ceil(N/4) - 1;
+[q_hat, r_hat] = h_estimation( trainingsymbols(end-(L+max(N_i)-1) + 1 : end), ...
+    r(end - 4*(L+max(N_i)-1) + 1: end), L, N_i);
+d_no_trans = r(end-length(r_hat)+1 : end);
+error_func(N) = sum(abs(r_hat - d_no_trans).^2)/length(r_hat); % TODO check if we should divide for L?
+
+q_hat_vec = reshape(q_hat, numel(q_hat), 1);
+figure, plot(0:length(q)-1, abs(q)), hold on, plot(0:length(q_hat_vec)-1, abs(q_hat_vec))
+legend('q', 'qhat'), grid on, xlim([-0.2, length(q) + 0.2])
+
+%% Estimate t0 as in pag 617 bc
+m_min = 0;
+m_max = 43;
+i = 1;
+crossvec = zeros(m_max - m_min, 1);
+for m = m_min:m_max
+    r_part = r(m+1:T:m+1+4*(L-1)); % pick L samples from r, spaced by T
+    crossvec(i) = abs(sum(r_part.*conj(trainingsymbols(1:L)))/L); % as in 7.269
+    i = i + 1;
+end
+
+figure, plot(m_min:m_max, crossvec), grid on
+
+[~, m_opt] = max(crossvec);
+m_opt = m_opt - 1; % beacuse of MATLAB indexing
+
+%% Interpolate qhat to get h_i
+init_offs = mod(m_opt, 4);
+hi = q_hat_vec(init_offs+1:4:end);
+
+figure, plot(0:length(q)-1, abs(q)), hold on, plot(0:length(q_hat_vec)-1, abs(q_hat_vec)), hold on,
+stem(init_offs:4:length(q), abs(hi))
+legend('q', 'qhat', 'hi'), grid on, xlim([-0.2, length(q) + 0.2])
