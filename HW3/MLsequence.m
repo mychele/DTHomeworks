3d0077c5e70b12ab05ef7041e6b9d2c0ca7e83e6
andrea-dittadi <andrea.dittadi@gmail.com>
2015-05-15 22:52:42 +0200

    Added 2^18-1 to MLsequence
    Former-commit-id: 1bdd16f8115e5777d5f62c4dd046de093363de30

--- HW3/MLsequence.m
+++ HW3/MLsequence.m
@@ -23,6 +23,8 @@ for l = r+1:(L) % Skip the initial condition for the cycle
             p(l) = xor(p(l-6), p(l-7));
         case 2^15 -1
             p(l) = xor(p(l-14), p(l-15));
+        case 2^18 -1
+            p(l) = xor(p(l-11), p(l-18));
         case 2^20 -1
             p(l) = xor(p(l-17), p(l-20));
     end
