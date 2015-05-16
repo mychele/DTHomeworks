8c728064c127c9b2144ce0423420946654327567
andrea-dittadi <andrea.dittadi@gmail.com>
2015-05-16 23:49:46 +0200

    channel_estimator: plot of error functional for choosing N1 once t0 (@T) is determined
    Former-commit-id: 1468116e195e03821972d6bfa4cc258fd842620e

--- HW3/channel_estimator.m
+++ HW3/channel_estimator.m
@@ -72,8 +72,8 @@ m_min = 0;
 m_max = 43;
 i = 1;
 crossvec = zeros(m_max - m_min, 1);
-for m = m_min:m_max
-    r_part = r(m+1:T:m+1+4*(L-1)); % pick L samples from r, spaced by T
+for m = m_min : m_max
+    r_part = r(m+1 : T : m+1+T*(L-1));  % pick L samples from r, spaced by T
     crossvec(i) = abs(sum(r_part.*conj(trainingsymbols(1:L)))/L); % as in 7.269
     i = i + 1;
 end
@@ -86,7 +86,8 @@ xlim([-0.2, m_max + 0.2])
 
 [~, m_opt] = max(crossvec);
 m_opt = m_opt - 1; % because of MATLAB indexing
-init_offs = mod(m_opt, 4);
+init_offs = mod(m_opt, T);
+t0 = floor(m_opt / T);   % this is t0 @ T
 
 
 
@@ -110,6 +111,26 @@ grid on
 
 % Fix N
 N = 7;
+
+
+%% Choose number of "anticausal" coefficients, given t0
+
+maxN1 = t0; % we already know it can't be larger than this
+error_func = zeros(maxN1, 1);
+for N1 = 0:maxN1
+    x_for_ls = trainingsymbols(end-(L+N-1) + 1 : end);
+    d_for_ls = r(end - 4*(L+N-1) + 1 - (length(q)-4) + init_offs + 4*t0: 4 :end - (length(q)-4) + 4*t0 + init_offs);
+    [~, r_hat] = h_estimation_onebranch(x_for_ls, d_for_ls, L, N-N1);    
+    d_no_trans = d_for_ls(N-N1 : N-N1+L-1);
+    error_func(N1+1) = sum(abs(r_hat - d_no_trans).^2)/length(r_hat);
+end
+
+figure
+plot(0:maxN1, 10*log10(error_func)), hold on, plot([0 maxN1], 10*log10(sigma_w*[1 1]))
+title('Error functional estimating h @T, given N and t_0')
+xlabel('N1_{T}'), ylabel('\epsilon [dB]')
+grid on, ylim([-25 -15])
+
 
 
 %% Estimate q_hat @T for the chosen N
@@ -150,10 +171,11 @@ title('q, qhat and hi')
 xlabel('iT/4')
 
 
-%% Possible way to define N1 and N2
-[h0, i0] = max(hi); % in this way i0 correspond to t0 + 0*T
-N1 = i0 - 1; % number of precursors
-N2 = length(hi) - i0; % number of postcursors
+%% Define N1 and N2
+
+[h0, i0] = max(hi);     % in this way i0 corresponds to t0 + 0*T
+N1 = i0 - 1;            % number of precursors
+N2 = length(hi) - i0;   % number of postcursors
 
 %%%%% TODO: remember!!! Divide your received signal and everything for h0
 %%%%% in order to get the same constellation of the tx at receiver
