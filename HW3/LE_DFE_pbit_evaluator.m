6d1731b085d96136beea6f2bfaae756d3414cff6
mychele <michele.polese@gmail.com>
2015-05-18 14:03:17 +0200

    Refactored LE_DFE pbit eval to work in T, modified pbit_blade

--- HW3/LE_DFE_pbit_evaluator.m
+++ HW3/LE_DFE_pbit_evaluator.m
@@ -1,15 +1,18 @@
-%% This script performs simulation of BER for LD, DFE
+%% This script performs simulation of BER for LE, DFE
 clear
 close all
 clc
 rng default
 
-Tc = 1;
-T = 4 * Tc;
-snr_vec = [5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15]; % dB
+T = 1;
+snr_vec = 6:2:14; %[5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15]; % dB
+% From exercise 1
+assumed_dly = 2;
+assumed_m_opt = 10;
+init_offs = mod(assumed_m_opt, 4);  % offset in T/4
+t0 = assumed_dly;                   % t0 is @ T; TODO this is N1, we should refactor
 
 %% LE pbit evaluation
-
 L_data = 2^20-1; % be patient
 
 sim_each = 64;
@@ -22,38 +25,38 @@ for snr_i = 1:length(snr_vec)
     snr_ch = snr_vec(snr_i);
     parfor sim = 1:sim_each
         % perform sim_each simulations
-        %% Create, send and receive data with the given channel, just once for each snr
-        T = 4*Tc;
-        [packet, r_T4, ~] = txrc(L_data, snr_ch, T, Tc);
         
-        % estimate the channel using the first 100 samples (4*length(ts))
-        N = 7; % fixed, as specified by the teacher
-        [ m_opt, h, est_sigmaw, N1, N2 ] = get_channel_info(r_T4(1:100), N, T);
+        % Create, send and receive data with the given channel
+        fprintf('Generating input symbols and channel output... ')
+        [packet, r, sigma_w] = txrc(L_data, snr_ch, assumed_m_opt);
+        fprintf('done!\n')
         
-        % sample to get r @ T
-        init_offs = mod(m_opt, 4); % in T/4
-        t0 = floor(m_opt/4); % from now consider T = 1
-        T = 1;
+        % Estimate the channel using the first 100 samples (4*length(ts))
+        fprintf('Estimating timing phase and IR... ')
+        [ h, est_sigmaw, N1, N2 ] = get_channel_info(r(init_offs+1:25+init_offs), 0, 4, assumed_m_opt);
+        fprintf('done!\n')
         
-        %% Detection begins
-        rT = r_T4(init_offs+1:4:end); % data sampled in T
-        x = rT(t0+1:t0+1+length(packet)-1)/h(N1+1).'; % data normalized by h0, starting from t0
-        hi = h/h(N1+1).'; % impulse response normalized by h0
+        % Sample to get r @ T
+        x = r / h(N1+1).';         % data normalized by h0
+        hi = h / h(N1+1).';         % impulse response normalized by h0
         
+        % Detection begins
         N = N1+N2+1;    % For each symbol, we have N-1 interferers + the symbol
         M1 = 20;        % FF filter: equal to the span of h
         D = 14;
         M2 = 0;      % FB filter not present in LE
         % Print progress update
-        fprintf('LE, snr = %d, M1 = %d, D = %d\n', snr_ch, M1, D);
-        % Compute only the functional
-        [~, pbit, num_err, ~] = DFE_filter(packet, x, hi, N1, N2, est_sigmaw, t0, D, M1, M2, 0);
+        fprintf('LE, snr = %d, M1 = %d, D = %d... ', snr_ch, M1, D);
+        % Compute
+        [~, pbit, num_err, ~] = DFE_filter(packet, x(1+assumed_dly : assumed_dly+length(packet)), hi, N1, N2, est_sigmaw, t0, D, M1, M2, 0);
         pbitLE(snr_i, sim) = pbit;
         num_bit_errorLE(snr_i, sim) = num_err;
+        fprintf('done!\n');
     end
 end
 
-save('pbit_LE', 'pbitLE', 'num_bit_errorLE');
+%save('pbit_LE', 'pbitLE', 'num_bit_errorLE');
+
 
 %% DFE pbit evaluation
 
@@ -64,36 +67,32 @@ for snr_i = 1:length(snr_vec)
     snr_ch = snr_vec(snr_i);
     % perform sim_each simulations
     parfor sim = 1:sim_each
-        %% Create, send and receive data with the given channel, just once for each snr
-        T = 4*Tc;
-        [packet, r_T4, ~] = txrc(L_data, snr_ch, T, Tc);
+        % Create, send and receive data with the given channel
+        fprintf('Generating input symbols and channel output... ')
+        [packet, r, sigma_w] = txrc(L_data, snr_ch, assumed_m_opt);
+        fprintf('done!\n')
         
-        % estimate the channel using the first 100 samples (4*length(ts))
-        N = 7; % fixed, as specified by the teacher
-        [ m_opt, h, est_sigmaw, N1, N2 ] = get_channel_info(r_T4(1:100), N, T);
+        % Estimate the channel using the first 100 samples (4*length(ts))
+        fprintf('Estimating timing phase and IR... ')
+        [ h, est_sigmaw, N1, N2 ] = get_channel_info(r(init_offs+1:25+init_offs), 0, 4, assumed_m_opt);
+        fprintf('done!\n')
         
-        % sample to get r @ T
-        init_offs = mod(m_opt, 4); % in T/4
-        t0 = floor(m_opt/4); % from now consider T = 1
-        T = 1;
+        % Sample to get r @ T
+        x = r / h(N1+1).';         % data normalized by h0
+        hi = h / h(N1+1).';         % impulse response normalized by h0
         
-        %% Detection begins
-        rT = r_T4(init_offs+1:4:end); % data sampled in T
-        x = rT(t0+1:t0+1+length(packet)-1)/h(N1+1).'; % data normalized by h0, starting from t0
-        hi = h/h(N1+1).'; % impulse response normalized by h0
-        
+        % Detection begins
         N = N1+N2+1;    % For each symbol, we have N-1 interferers + the symbol
         M1 = 20;        % FF filter: equal to the span of h
         D = M1-1;
         M2 = N2 + M1 - 1 - D;  % FB filter
         % Print progress update
-        % Print progress update
         fprintf('DFE, snr = %d, M1 = %d, D = %d\n', snr_ch, M1, D);
-        
-        % Compute only the functional
-        [~, pbit, num_err, ~] = DFE_filter(packet, x, hi, N1, N2, est_sigmaw, t0, D, M1, M2, 0);
+        % Compute
+        [~, pbit, num_err, ~] = DFE_filter(packet, x(1+assumed_dly : assumed_dly+length(packet)), hi, N1, N2, est_sigmaw, t0, D, M1, M2, 0);
         pbitDFE(snr_i, sim) = pbit;
         num_bit_errorDFE(snr_i, sim) = num_err;
+        fprintf('done!\n');
     end
 end
 
@@ -103,8 +102,8 @@ delete(gcp);
 
 %% Statistics
 
-BER_LE = median(pbitLE, 2);
-BER_DFE = median(pbitDFE, 2);
+BER_LE = mean(pbitLE, 2);
+BER_DFE = mean(pbitDFE, 2);
 
 BER_ideal = BER_awgn(snr_vec);
 
