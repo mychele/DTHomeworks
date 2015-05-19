68e8c2ac168d08bdaa480e1d07c60415f4d0f13d
mychele <michele.polese@gmail.com>
2015-05-19 16:08:08 +0200

    2 nasty bugs fixed! Yo!

--- HW3/fba.m
+++ HW3/fba.m
@@ -1,9 +1,11 @@
+% TODO functionise me
+
 %function [ detected, pbit ] = fba( packet, r, hi, L1, L2 ) %#ok<INUSL,STOUT>
-clear all
-receiver_util 
+clear
+receiver_util
 close all
 clc
-r = x(1+assumed_dly:end);
+r = x(1+assumed_dly:1+assumed_dly+length(packet)-1);
 M = 4;
 L1 = 0;
 L2 = 4;
@@ -21,46 +23,50 @@ L2 = 4;
 
 % The desired signal u_k can be obtained as linear combination of s_k
 % and s_k-1
-
+% tic
 % Initialization
 Ns = M^(L1+L2); % Number of states
 K = length(r);
 symb = [1+1i, 1-1i, -1+1i, -1-1i]; % Possible transmitted symbols (QPSK)
 
 % -- Define u_mat matrix
-ndigits = L1 + L2; % n digits of the state
-statevec = zeros(1, ndigits); % symbol index, from the oldest to the newest
-i = ndigits;
 states_symbols = zeros(Ns, M);
+statelength = L1 + L2; % number of digits of the state, i.e. its length in base M=4
+statevec = zeros(1, statelength); % symbol index, from the oldest to the newest
 u_mat = zeros(Ns, M);
 for state = 1:Ns
+    % Set value of the current element of u_mat
     for j = 1:M
-        lastsymbols = [symb(statevec + 1), symb(j)];   % symbols, from the oldest to the newest
+        lastsymbols = [symb(statevec + 1), symb(j)]; % symbols, from the oldest to the newest
         u_mat(state, j) = lastsymbols * flipud(hi);
-    end
-    
+    end    
     states_symbols(state,:) = lastsymbols(1:4);
-    
     % Update statevec
-    statevec(i) = statevec(i) + 1;
-    l = i;
-    while (statevec(l) >= M && l > 1)
-        statevec(l) = 0;
-        l = l-1;
-        statevec(l) = statevec(l) + 1;
+    statevec(statelength) = statevec(statelength) + 1;
+    i = statelength;
+    while (statevec(i) >= M && i > 1)
+        statevec(i) = 0;
+        i = i-1;
+        statevec(i) = statevec(i) + 1;
     end
 end
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 % Channel transition metrics computation
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+% TODO options: operate on windows, therefore c can be handle and
+% precomputed OR try to define it ONLY for the states for which there's a
+% transition
+
 c = -1000*ones(Ns, Ns, K+1);
+fprintf('channel transition metric...')
 for k = 1:K
     newstate = 0;
     
     for state = 1:Ns
         % Iterate over the branches
-        for j = 1:M    
+        for j = 1:M
             newstate = newstate + 1;
             if newstate > Ns, newstate = 1; end
             
@@ -71,60 +77,75 @@ for k = 1:K
     end
 end
 c(:,:,K+1) = 0;
+fprintf('done\n')
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 % Backward metric computation
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 b = zeros(Ns, K+1);   % This will also initialize the last state
-
+fprintf('bck...')
 for k = K:-1:1
-    
-    newstate = 0;
-    
-    for i = 1:Ns
         
-        curr_bkw_metric = zeros(1,Ns);
+    for i = 1:Ns
+        % TODO check if correct!
+        b(i, k) = max(b(1:Ns, k+1) + c(1:Ns, i, k+1));
         
         % Iterate over the trellis transitions for the current state
-        for m = 1:Ns
-            
-            % Compute backward metric for each of the new states
-            curr_bkw_metric(m) = b(m, k+1) + c(m, i, k+1);
-        end
+%         for m = 1:Ns
+%             
+%             % Compute backward metric for each of the new states
+%             curr_bkw_metric(m) = b(m, k+1) + c(m, i, k+1);
+%         end
         
         % Only keep the maximum among the bkw metrics
-        b(i,k) = max(curr_bkw_metric);
+%         b(i,k) = max(curr_bkw_metric);
     end
 end
+fprintf('done\n')
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%
 % Forward metric computation
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%
-f = zeros(Ns, K+2);   % This will also initialize the initial state condition
-for k = 2:K+1   % F_(-1) is the initial condition!
+f = zeros(Ns, K);   % This will also initialize the initial state condition
+fprintf('fwd...')
+% for k = 1 (i.e. 0 in reality)
+for j = 1:Ns
     
-    for j = 1:Ns
-        
-        curr_fwd_metric = zeros(1,Ns);
-        
-        % Iterate over the branches
-        for l = 1:Ns
-            % Compute backward metric for each of the new states
-            curr_fwd_metric(l) = f(l, k-1) + c(j, l, k);
-        end
-        
-        % Only keep the maximum among the bkw metrics
-        f(j,k) = max(curr_fwd_metric);
-    end
+%     curr_fwd_metric = zeros(1,Ns);
+%     
+%     % Iterate over the branches
+%     for l = 1:Ns
+%         % Compute backward metric for each of the new states
+%         curr_fwd_metric(l) = 0 + c(j, l, 1); 
+%     end
+    
+    % Only keep the maximum among the bkw metrics
+    f(j,1) = max(0 + c(j, 1:Ns, 1)); % 0 until we don't have smth better
 end
 
-% State metric computation
-v = zeros(Ns, K);
-for k = 1:K
-    for i = 1:Ns
-        v(i, k) = f(i, k+1) + b(i, k);
+for k = 2:K   % F_(-1) is the initial condition!
+    for j = 1:Ns
+%         curr_fwd_metric = zeros(1,Ns);
+%         
+%         % Iterate over the branches
+%         for l = 1:Ns
+%             % Compute backward metric for each of the new states
+%             curr_fwd_metric(l) = f(l, k-1) + c(j, l, k);
+%         end
+%         
+        % Only keep the maximum among the bkw metrics
+        f(j,k) = max(f(1:Ns, k-1) + c(j, 1:Ns, k).');
     end
 end
+fprintf('done\n')
+
+% State metric computation
+v = f+b(:, 1:end-1); %zeros(Ns, K);
+% for k = 1:K
+%     for i = 1:Ns
+%         v(i, k) = f(i, k) + b(i, k);
+%     end
+% end
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 % Likelihood function computation
@@ -133,20 +154,19 @@ end
 l = zeros(M, K);
 for k = 1:K
     for beta = 1:M
-        l(beta, k) = max(v(find(states_symbols(:,1) == symb(beta)),k));
+        l(beta, k) = max(v(find(states_symbols(:,M) == symb(beta)),k));
     end
 end
 
 % Decision
-[maximum, maxind] = max(l);
+[~, maxind] = max(l);
 decisions = symb(maxind);
-
+% disp(toc)
 
 % Pbit computation
-detected = decisions(3:end-2);
-[pbit, num_bit_errors] = BER(packet, detected);
+[pbit, num_bit_errors] = BER(packet, decisions);
 
-num_errors = sum(packet-detected.' ~= 0);
+num_errors = sum(packet-decisions.' ~= 0);
 fprintf('P_err = %.g (%d errors)\n', num_errors / length(packet), num_errors)
 fprintf('P_bit = %.g (%d errors)\n', pbit, num_bit_errors)
 
