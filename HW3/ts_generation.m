a109f9cf6f1d55b9cb5cdcc9ade47950a31f45de
DvdMgr <magrin.davide@gmail.com>
2015-05-21 17:42:37 +0200

    Minor comments

--- HW3/ts_generation.m
+++ HW3/ts_generation.m
@@ -1,10 +1,11 @@
 function [ trainingsymbols ] = ts_generation( L, Nseq )
 % Training sequence generation
+% This function outputs a partially repeated ML sequence in which the 
+% symbols are 1+j and -1-j (two orthogonal symbols from the QPSK alphabet).
 
-mlseq = MLsequence(L);
+mlseq = MLsequence(L); % Get the 0-1 ML sequence
 
-% Replace every 0 with two 0s and every 1 with two 1s to put it into the
-% bitmap
+% Replace every 0 with two 0s and every 1 with two 1s 
 mlseqdouble = zeros(2*L,1);
 for i = 1:L
     switch mlseq(i)
@@ -21,6 +22,4 @@ end
 trainingseq = [mlseqdouble; mlseqdouble(1:2*Nseq)];
 trainingsymbols = bitmap(trainingseq);
 
-
 end
-
