0bed38c23142d7120729390aafc171675f1015c1
DvdMgr <magrin.davide@gmail.com>
2015-05-23 23:25:05 +0200

    Added titles. Titles are good.

--- HW3/BER_plot.m
+++ HW3/BER_plot.m
@@ -23,6 +23,7 @@ semilogy(snr_vec, pbit_AWGN_sim)
 xlabel('snr [dB]'), ylabel('BER')
 legend('LE, M1 = 20, D = 15', 'DFE, M1 = 25, D = 24, M2 = 4', 'Viterbi', 'FBA', 'AWGN', 'AWGN with simulation')
 ylim([10^-5.5, 10^-1]), grid on
+title('BER for one realization, estimated channel');
 
 
 %% Statistics for real channel
@@ -31,7 +32,6 @@ load('pbit_LE_real_channel.mat');
 load('pbit_DFE_real_channel.mat');
 load('pbit_fba_real_channel.mat');
 load('pbit_viterbi_realch.mat')
-
 
 figure
 semilogy(snr_vec, pbitLE_real_channel), hold on
@@ -43,6 +43,7 @@ semilogy(snr_vec, pbit_AWGN_sim)
 xlabel('snr [dB]'), ylabel('BER')
 legend('LE, M1 = 20, D = 15', 'DFE, M1 = 25, D = 24, M2 = 4', 'Viterbi', 'FBA', 'AWGN', 'AWGN with simulation')
 ylim([10^-7, 10^-1]), xlim([5, 15]), grid on
+title('BER for methods using the actual channel impulse response');
 
 %% Statistics for same estimate
 
@@ -51,16 +52,19 @@ snr_vec = 5:13;
 load('pbit_LE_same_channel.mat');
 load('pbit_DFE_same_channel.mat');
 load('pbit_viterbi_same_ch513.mat');
+load('pbit_fba_same_channel.mat');
 
 figure
 semilogy(snr_vec, pbitLE_same_channel), hold on,
 semilogy(snr_vec, pbitDFE_same_channel)
 semilogy(snr_vec_viterbi, pbit_viterbi_same_ch)
+semilogy(snr_vec_fba_same_channel, pbit_fba_same_channel)
 semilogy(snr_vec, BER_awgn(snr_vec))
 semilogy(snr_vec, pbit_AWGN_sim(1:9))
 xlabel('snr [dB]'), ylabel('BER')
-legend('LE, M1 = 20, D = 15', 'DFE, M1 = 25, D = 24, M2 = 4', 'FBA', 'AWGN', 'AWGN with simulation')
+legend('LE, M1 = 20, D = 15', 'DFE, M1 = 25, D = 24, M2 = 4', 'Viterbi', 'FBA', 'AWGN', 'AWGN with simulation')
 ylim([10^-7, 10^-1]), xlim([5, 15]), grid on
+title('BER for various methods, same channel noise and estimate');
 
 
 %% Statistics for estimated channel, average over independent experiments
@@ -77,3 +81,4 @@ semilogy(snr_vec, pbit_AWGN_sim)
 xlabel('snr [dB]'), ylabel('BER')
 legend('LE, M1 = 20, D = 15', 'DFE, M1 = 25, D = 24, M2 = 4', 'Viterbi', 'FBA', 'AWGN', 'AWGN with simulation')
 ylim([10^-5.5, 10^-1]), grid on
+title('Average BER for independent experiments, estimated channel');
