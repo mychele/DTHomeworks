ec2f54dd2522b7d32c7a28c881d39b526a846122
andrea-dittadi <andrea.dittadi@gmail.com>
2015-05-23 19:24:00 +0200

    Fixed matrix dimensions in pbit_fba_real_channel

--- HW3/BER_plot.m
+++ HW3/BER_plot.m
@@ -1,3 +1,5 @@
+clear, clc, close all
+
 %% Group all the BER curves
 
 load('pbit_DFE.mat');
@@ -13,10 +15,10 @@ snr_vec = 5:15;
 
 figure
 semilogy(snr_vec, pbitLE(:, 3)), hold on,
-semilogy(snr_vec, pbitDFE(:, 2)), hold on, 
-semilogy(snr_vec_viterbi, pbit_viterbi(:, 3)), hold on,
-semilogy(snr_vec, pbit_fba), hold on,
-semilogy(snr_vec, BER_awgn(snr_vec)), hold on,
+semilogy(snr_vec, pbitDFE(:, 2))
+semilogy(snr_vec_viterbi, pbit_viterbi(:, 3))
+semilogy(snr_vec, pbit_fba)
+semilogy(snr_vec, BER_awgn(snr_vec))
 semilogy(snr_vec, pbit_AWGN_sim)
 xlabel('snr [dB]'), ylabel('BER')
 legend('LE, M1 = 20, D = 15', 'DFE, M1 = 25, D = 24, M2 = 4', 'Viterbi', 'FBA', 'AWGN', 'AWGN with simulation')
@@ -32,11 +34,11 @@ load('pbit_viterbi_realch.mat')
 
 
 figure
-semilogy(snr_vec, pbitLE_real_channel), hold on,
-semilogy(snr_vec, pbitDFE_real_channel), hold on, 
-semilogy(snr_vec_viterbi, pbit_viterbi), hold on,
-semilogy(snr_vec_fba_real_channel, pbit_fba_real_channel), hold on,
-semilogy(snr_vec, BER_awgn(snr_vec)), hold on,
+semilogy(snr_vec, pbitLE_real_channel), hold on
+semilogy(snr_vec, pbitDFE_real_channel)
+semilogy(snr_vec_viterbi, pbit_viterbi)
+semilogy(snr_vec_fba_real_channel, pbit_fba_real_channel)
+semilogy(snr_vec, BER_awgn(snr_vec))
 semilogy(snr_vec, pbit_AWGN_sim)
 xlabel('snr [dB]'), ylabel('BER')
 legend('LE, M1 = 20, D = 15', 'DFE, M1 = 25, D = 24, M2 = 4', 'Viterbi', 'FBA', 'AWGN', 'AWGN with simulation')
@@ -52,10 +54,9 @@ load('pbit_viterbi_same_ch513.mat');
 
 figure
 semilogy(snr_vec, pbitLE_same_channel), hold on,
-semilogy(snr_vec, pbitDFE_same_channel), hold on, 
-semilogy(snr_vec_viterbi, pbit_viterbi_same_ch), hold on,
-%semilogy(snr_vec_fba_real_channel + 0.3, pbit_fba_real_channel), hold on,
-semilogy(snr_vec, BER_awgn(snr_vec)), hold on,
+semilogy(snr_vec, pbitDFE_same_channel)
+semilogy(snr_vec_viterbi, pbit_viterbi_same_ch)
+semilogy(snr_vec, BER_awgn(snr_vec))
 semilogy(snr_vec, pbit_AWGN_sim(1:9))
 xlabel('snr [dB]'), ylabel('BER')
 legend('LE, M1 = 20, D = 15', 'DFE, M1 = 25, D = 24, M2 = 4', 'FBA', 'AWGN', 'AWGN with simulation')
