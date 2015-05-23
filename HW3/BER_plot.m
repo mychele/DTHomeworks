7b7c30bea7825ef303a3f36bfdd7dc9439735640
mychele <michele.polese@gmail.com>
2015-05-23 15:44:54 +0200

    Added section for BER of same estimate channel

--- HW3/BER_plot.m
+++ HW3/BER_plot.m
@@ -34,14 +34,29 @@ load('pbit_viterbi_realch.mat')
 figure
 semilogy(snr_vec, pbitLE_real_channel), hold on,
 semilogy(snr_vec, pbitDFE_real_channel), hold on, 
-semilogy(snr_vec_viterbi, pbit_viterbi), hold on,
-semilogy(snr_vec_fba_real_channel, pbit_fba_real_channel), hold on,
+semilogy(snr_vec_viterbi + 0.3, pbit_viterbi), hold on,
+semilogy(snr_vec_fba_real_channel + 0.3, pbit_fba_real_channel), hold on,
 semilogy(snr_vec, BER_awgn(snr_vec)), hold on,
 semilogy(snr_vec, pbit_AWGN_sim)
 xlabel('snr [dB]'), ylabel('BER')
 legend('LE, M1 = 20, D = 15', 'DFE, M1 = 25, D = 24, M2 = 4', 'FBA', 'AWGN', 'AWGN with simulation')
-ylim([10^-7, 10^-1]), grid on
+ylim([10^-7, 10^-1]), xlim([5, 15]), grid on
 
+%% Statistics for same estimate
 
+snr_vec = 5:13; 
 
+load('pbit_LE_same_channel.mat');
+load('pbit_DFE_same_channel.mat');
+load('pbit_viterbi_same_ch513.mat');
 
+figure
+semilogy(snr_vec, pbitLE_same_channel), hold on,
+semilogy(snr_vec, pbitDFE_same_channel), hold on, 
+semilogy(snr_vec_viterbi, pbit_viterbi_same_ch), hold on,
+%semilogy(snr_vec_fba_real_channel + 0.3, pbit_fba_real_channel), hold on,
+semilogy(snr_vec, BER_awgn(snr_vec)), hold on,
+semilogy(snr_vec, pbit_AWGN_sim(1:9))
+xlabel('snr [dB]'), ylabel('BER')
+legend('LE, M1 = 20, D = 15', 'DFE, M1 = 25, D = 24, M2 = 4', 'FBA', 'AWGN', 'AWGN with simulation')
+ylim([10^-7, 10^-1]), xlim([5, 15]), grid on
