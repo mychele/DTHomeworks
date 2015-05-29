77670011185d6ae7df9f6ff7928f9c819f4127da
andrea-dittadi <andrea.dittadi@gmail.com>
2015-05-29 17:01:27 +0200

    Created script for problem 1: Pbit estimation with ideal channel, both with and without coding

---
+++ HW4/problem_1.m
@@ -0,0 +1,91 @@
+% This script solves the first problem.
+clear
+close all
+clc
+
+rng default
+
+%parpool(4);
+
+%% Get optimal number of bits
+desired_bits = 2^22;
+% Compute the closest number of bits that the encoder will like
+found = false;
+bit_number = 0;
+while(~found)
+   search_step = 32400;
+   bit_number = bit_number + search_step;
+   if (bit_number > desired_bits)
+      found = true;
+   end
+end
+
+%% Estimate Pbit for ideal channel without encoding
+snr_vec = 0:0.5:14;
+Pbit_noenc = zeros(1,length(snr_vec));
+
+for curr_snr = 1:length(snr_vec)
+   disp(curr_snr);
+   snrdb = snr_vec(curr_snr);
+   % Simulate Pbit for the ideal channel, no encoding
+   bits = randi([0 1], 1, bit_number);
+   symbols = bitmap(bits.');
+   
+   % Send stuff through
+   snrlin = 10^(snrdb/10);
+   Eh = 1;  % Energy of the ideal channel ir
+   sigma_w = 2*Eh/snrlin;
+   w = wgn(length(symbols), 1, 10*log10(sigma_w), 'complex');
+   rcv_bits = symbols + w;
+   
+   % Threshold the bits
+   decided_symbols = zeros(1, length(rcv_bits));
+   for idx = 1:length(rcv_bits)
+      decided_symbols(idx) = qpsk_td(rcv_bits(idx));  % TODO modify qpsk_td to handle vectors
+   end
+   
+   decided_bits = ibmap(decided_symbols);
+   
+   % Estimate pbit
+   Pbit_noenc(curr_snr) = sum(xor(decided_bits.', bits))/length(bits);
+   
+end
+   
+%% Estimate Pbit for ideal channel with encoding
+snr_vec_enc = 0:0.1:1;
+Pbit_enc = zeros(1,length(snr_vec_enc));
+
+for curr_snr = 1:length(snr_vec_enc)
+   disp(snr_vec_enc(curr_snr));
+   snrdb = snr_vec_enc(curr_snr);
+   bits = randi([0 1], 1, bit_number);
+   
+   enc_bits = encodeBits(bits);
+   int_enc_bits = interleaver(enc_bits);  % Interleave the encoded bits
+   
+   symbols = bitmap(int_enc_bits.');
+   
+   % Send stuff through
+   snrlin = 10^(snrdb/10);
+   Eh = 1;
+   sigma_w = 2*Eh/snrlin;
+   w = wgn(length(symbols), 1, 10*log10(sigma_w), 'complex');
+   rcv_bits = symbols + w;
+   
+   % Compute Log Likelihood Ratio
+   llr = zeros(2*length(symbols),1);
+   llr(1:2:end) = -2*real(rcv_bits)/(sigma_w/2);
+   llr(2:2:end) = -2*imag(rcv_bits)/(sigma_w/2);
+   
+   % Decode the bits
+   llr = deinterleaver(llr); % Deinterleave the loglikelihood ratio first
+   dec_bits = decodeBits(llr).';
+   
+   Pbit_enc(curr_snr) = sum(xor(dec_bits, bits))/length(bits);
+   
+end
+
+%% Plot results
+semilogy(snr_vec, Pbit_noenc), hold on,
+semilogy(snr_vec_enc, Pbit_enc)
+ylim([10^-5, 10^-1]), xlim([0, 14]), grid on
