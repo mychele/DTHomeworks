9fbdef1e9b84242e0429d6796d5bccc8fba98948
mychele <michele.polese@gmail.com>
2015-05-31 17:04:36 +0200

    Script that calls OFDM function, simulation results

--- HW4/OFDM_util.m
+++ HW4/OFDM_util.m
@@ -1,107 +1,47 @@
-% ODFM transmitter
-
+%% ODFM util script, it calls the OFDM function
 clear
 close all
 clc
-
 rng default
-% The design of the DFE equalizer has to be carried out assuming the
-% channel is known
-% From the assigned impulse response
-t0 = 6;
-N1 = 0;
-N2 = 4;
 
-% Data
+parpool(4); 
+
+%% Data
 M = 512;
 Npx = 7;
-OFDM = true;
-%% Get optimal number of bits
-desired_bits = 2^18;
-% Compute the closest number of bits that both interleaver and encoder will
-% like
-found = false;
-bit_number = 0;
-while(~found)
-    search_step = 32400;
-    bit_number = bit_number + search_step;
-    if (bit_number > desired_bits)
-        found = true;
-    end
+desired_bits = 2^20;
+
+%% BER with coding
+coding = true;
+snr_vec_coding = 1:0.05:2;
+BER_coding = zeros(length(snr_vec_coding), 1);
+parfor snr_i = 1:length(snr_vec_coding)
+    snr_c = snr_vec_coding(snr_i);
+    fprintf('Coded, snr = %d\n', snr_c);
+    [BER_coding(snr_i), ~] = OFDM_BER(M, Npx, desired_bits, snr_c, coding);
 end
 
-%% Generate and encode bits
-bits = randi([0 1], 1, bit_number);
+%% BER without coding
+coding = false;
+snr_vec_nocoding = 0:15;
+BER_nocoding = zeros(length(snr_vec_nocoding), 1);
+parfor snr_i = 1:length(snr_vec_nocoding)
+    snr_nc = snr_vec_nocoding(snr_i);
+    fprintf('Uncoded, snr = %d\n', snr_nc);
+    [BER_nocoding(snr_i), ~] = OFDM_BER(M, Npx, desired_bits, snr_nc, coding);
+end
 
-enc_bits = encodeBits(bits);
+delete(gcp);
+%% Plot
 
-int_enc_bits = interleaver(enc_bits);  % Interleave the encoded bits
-coding = true;
+figure, 
+semilogy(snr_vec_coding, BER_coding),
+hold on,
+semilogy(snr_vec_nocoding, BER_nocoding)
 
-a = bitmap(int_enc_bits.');
 
-%% Create data blocks
 
-% perform a zero padding of the last symbols in order to have blocks of 512
-% symbols
-% TODO consider how can this affect BER
-a_pad = [a; zeros(M - mod(length(a), M), 1)];
-a_matrix = reshape(a_pad, M, []); % it should mantain columnwise order
 
-% compute the ifft of blocks of 512 symbols
-% http://it.mathworks.com/matlabcentral/newsreader/view_thread/17104
-% it should be a columnwise operation!
-A_matrix = ifft(a_matrix); % TODO check if matlab downscales
-%clear a_matrix % we care about memory consumption, don't we? 
-
-% add the preamble to each column
-A_matrix = [A_matrix(M-Npx+1:M, :); A_matrix]; % very powerful operation
-
-% serialize in order to call channel output
-s = reshape(A_matrix, [], 1);
-% apply the correct gain too
-s = sqrt(M)*s;
-
-%% Send over the noisy channel
-% because of 9.35, 9.38 and 9.39
-snr = 10; % dB
-snr_lin = 10^(snr/10);
-[r, sigma_w, g] = channel_output(s, snr_lin, false);
-G = fft(g, 512);
-G = G(:);
-
-%% Process at the receiver
-%r = r(t0:end); % consider the effect of the convolution at the end should be easy,
-% since the resulting data should have a length which is a multiple of M +
-% Npx
-r = r(1:end - mod(length(r), M+Npx)); 
-
-% perform the DFT
-r_matrix = reshape(r, M+Npx, []);
-r_matrix = r_matrix(Npx + 1:end, :);
-
-G_i = G.^(-1);
-x_matrix = fft(r_matrix);
-
-y_matrix = bsxfun(@times, x_matrix, G_i);
-
-y = reshape(y_matrix, [], 1);
-
-% drop the zero padding
-y = y(1:length(a));
-
-%% Compute Log Likelihood Ratio
-llr = zeros(2*length(y),1);
-llr(1:2:end) = -2*real(y)/(sigma_w/2);
-llr(2:2:end) = -2*imag(y)/(sigma_w/2);
-
-%% Decode the bits
-
-llr = deinterleaver(llr); % Deinterleave the loglikelihood ratio first
-
-dec_bits = decodeBits(llr).';
-
-Pbit = sum(xor(dec_bits, bits))/length(bits);
 
 
 
