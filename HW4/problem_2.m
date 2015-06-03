f939f719b6ff1831f13df8bdefbf83932bab8ef1
DvdMgr <magrin.davide@gmail.com>
2015-06-03 14:36:37 +0200

    Setup for blade simulation

--- HW4/problem_2.m
+++ HW4/problem_2.m
@@ -5,7 +5,7 @@ clc
 
 rng default
 
-parpool(4);
+parpool(15);
 
 %% Known channel, DFE, uncoded data (HW3)
 
@@ -47,7 +47,7 @@ end
 
 % Save current results
 save('Problem2_knownch_uncoded', 'snr_vec_knownch_uncoded', ...
-   'seq_lengths_knownch_uncoded', 'Pbit_knownch_uncoded');
+    'seq_lengths_knownch_uncoded', 'Pbit_knownch_uncoded');
 
 %% Known channel, DFE, coded data
 % The design of the DFE equalizer has to be carried out assuming the
@@ -63,7 +63,7 @@ desired_bits = 2^22;
 search_step = 32400;
 bit_number = ceil(desired_bits / search_step) * search_step;
 
-snr_vec_knownch_coded = [1, 1.5, 2:0.1:2.5];  % Pbit falls at 2.2 dBs
+snr_vec_knownch_coded = [1, 1.5, 2:0.02:2.4];  % Pbit falls at 2.2 dBs
 seq_lengths_knownch_coded = bit_number*ones(1, length(snr_vec_knownch_coded));
 Pbit_knownch_coded = zeros(length(snr_vec_knownch_coded),1);
 
@@ -107,7 +107,7 @@ end
 
 % Save current results
 save('Problem2_knownch_coded', 'snr_vec_knownch_coded', ...
-   'seq_lengths_knownch_coded', 'Pbit_knownch_coded');
+    'seq_lengths_knownch_coded', 'Pbit_knownch_coded');
 
 
 %% Estimated channel, DFE, uncoded data
@@ -153,7 +153,7 @@ end
 
 % Save current results
 save('Problem2_estch_uncoded', 'snr_vec_estch_uncoded', ...
-   'seq_lengths_estch_uncoded', 'Pbit_estch_uncoded');
+    'seq_lengths_estch_uncoded', 'Pbit_estch_uncoded');
 
 %% Estimated channel, DFE, coded data
 % In this section we don't use the txrc function for now. This is because
@@ -175,54 +175,57 @@ desired_bits = 2^22;
 search_step = 32400;
 bit_number = ceil(desired_bits / search_step) * search_step;
 
-snr_vec_estch_coded = [1, 2, 3, 3.2:0.05:3.6];  % Pbit falls at 3.5 dB
+numsim = 10;
+
+snr_vec_estch_coded = [1, 2, 3, 3.2:0.02:3.6];  % Pbit falls at 3.5 dB
 seq_lengths_estch_coded = bit_number*ones(1, length(snr_vec_estch_coded));
-Pbit_estch_coded = zeros(length(snr_vec_estch_coded),1);
+Pbit_estch_coded = zeros(length(snr_vec_estch_coded),numsim);
 
-parfor snr_idx = 1:length(snr_vec_estch_coded)
-    curr_snr = snr_vec_estch_coded(snr_idx);
-    fprintf('Estimated channel, coded, snr = %.2f\n', curr_snr);
-    
-    % Generate the current needed sequence
-    packet = randi([0 1], 1, seq_lengths_estch_coded(snr_idx));
-    
-    enc_packet = encodeBits(packet);
-    int_enc_packet = interleaver(enc_packet);  % Interleave the encoded bits
-    
-    symbols = [ts_generation(L, Nseq); bitmap(int_enc_packet.')];
-    
-    % Send through the channel
-    [rcv_symb, sigma_w, ~] = channel_output(symbols, 10^(curr_snr/10), false);
-    
-    % Perform estimation
-    [h, est_sigma_w] = get_channel_info(rcv_symb(t0:t0+L+Nseq-1), N1, N2);
-    
-    rcv_symb = rcv_symb(t0:end-7)/h(N1+1);
-    hi = h / h(N1+1);
-    
-    % Receiver: filter with DFE
-    M1_dfe = 15;
-    D_dfe = M1_dfe - 1;
-    M2_dfe = N2 + M1_dfe - 1 - D_dfe;
-    [~, rcv_bits] = DFE_filter(rcv_symb, hi, N1, N2, est_sigma_w, D_dfe, M1_dfe, M2_dfe, true, false);
-    
-    % Compute Log Likelihood Ratio
-    llr = zeros(2*length(packet),1);
-    llr(1:2:end) = -2*real(rcv_bits(L+Nseq+1:end))/(est_sigma_w/2);
-    llr(2:2:end) = -2*imag(rcv_bits(L+Nseq+1:end))/(est_sigma_w/2);
-   
-    llr = deinterleaver(llr); % Deinterleave the loglikelihood ratio first
-   
-    dec_packet = decodeBits(llr).';
-    
-    % Compute the Pbit and store it
-    Pbit_estch_coded(snr_idx) = sum(xor(dec_packet, packet))/length(packet);
-    
+for sim = 1:numsim
+    parfor snr_idx = 1:length(snr_vec_estch_coded)
+        curr_snr = snr_vec_estch_coded(snr_idx);
+        fprintf('Estimated channel, coded, snr = %.2f\n', curr_snr);
+        
+        % Generate the current needed sequence
+        packet = randi([0 1], 1, seq_lengths_estch_coded(snr_idx));
+        
+        enc_packet = encodeBits(packet);
+        int_enc_packet = interleaver(enc_packet);  % Interleave the encoded bits
+        
+        symbols = [ts_generation(L, Nseq); bitmap(int_enc_packet.')];
+        
+        % Send through the channel
+        [rcv_symb, sigma_w, ~] = channel_output(symbols, 10^(curr_snr/10), false);
+        
+        % Perform estimation
+        [h, est_sigma_w] = get_channel_info(rcv_symb(t0:t0+L+Nseq-1), N1, N2);
+        
+        rcv_symb = rcv_symb(t0:end-7)/h(N1+1);
+        hi = h / h(N1+1);
+        
+        % Receiver: filter with DFE
+        M1_dfe = 15;
+        D_dfe = M1_dfe - 1;
+        M2_dfe = N2 + M1_dfe - 1 - D_dfe;
+        [~, rcv_bits] = DFE_filter(rcv_symb, hi, N1, N2, est_sigma_w, D_dfe, M1_dfe, M2_dfe, true, false);
+        
+        % Compute Log Likelihood Ratio
+        llr = zeros(2*length(packet),1);
+        llr(1:2:end) = -2*real(rcv_bits(L+Nseq+1:end))/(est_sigma_w/2);
+        llr(2:2:end) = -2*imag(rcv_bits(L+Nseq+1:end))/(est_sigma_w/2);
+        
+        llr = deinterleaver(llr); % Deinterleave the loglikelihood ratio first
+        
+        dec_packet = decodeBits(llr).';
+        
+        % Compute the Pbit and store it
+        Pbit_estch_coded(snr_idx, sim) = sum(xor(dec_packet, packet))/length(packet);
+        
+    end
 end
-
 % Save current results
 save('Problem2_estch_coded', 'snr_vec_estch_coded', ...
-   'seq_lengths_estch_coded', 'Pbit_estch_coded');
+    'seq_lengths_estch_coded', 'Pbit_estch_coded');
 
 %% Plot BER graphs
 
