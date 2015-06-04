b6f4b002096b951f4cee019806b73b2de2313ad7
mychele <michele.polese@gmail.com>
2015-06-04 10:07:33 +0200

    Script to plot BER for OFDM

---
+++ HW4/OFDM_BER_plot.m
@@ -0,0 +1,18 @@
+%% Load OFDM BER
+load('OFDM_uncoded_estimated.mat');
+load('OFDM_coded_estimated.mat');
+load('OFDM_coded_known.mat');
+load('OFDM_uncoded_known.mat');
+
+%% Plot
+figure,
+semilogy(snr_vec_coding_known, BER_coding_known), hold on,
+semilogy(snr_vec_nocoding_known, BER_nocoding_known), hold on,
+semilogy(snr_vec_coding_estimated, BER_coding_estimated), hold on,
+semilogy(snr_vec_nocoding_estimated, BER_nocoding_estimated)
+legend('OFDM, known channel, coded', 'OFDM, known channel, uncoded', ...
+    'OFDM, estimated channel, coded', 'OFDM, estimated channel, uncoded')
+xlabel('SNR')
+ylabel('BER')
+grid on
+title('BER for OFDM')
