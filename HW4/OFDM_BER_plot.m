cb71356c7da400b8f706f2a831bfe95088e50a7d
mychele <michele.polese@gmail.com>
2015-06-07 10:26:18 +0200

    Minor detail in OFDM_BER_PLOT

--- HW4/OFDM_BER_plot.m
+++ HW4/OFDM_BER_plot.m
@@ -8,13 +8,13 @@ load('OFDM_uncoded_known.mat');
 figure,
 semilogy(snr_vec_coding_known, BER_coding_known), hold on,
 semilogy(snr_vec_nocoding_known, BER_nocoding_known), hold on,
-semilogy(snr_vec_coding_estimated, BER_coding_estimated(:, 2), '--'), hold on,
+semilogy(snr_vec_coding_estimated, BER_coding_estimated(:, 5), '--'), hold on,
 semilogy(snr_vec_nocoding_estimated, BER_nocoding_estimated(:, 7), '--')
 legend('OFDM, known channel, coded', 'OFDM, known channel, uncoded', ...
     'OFDM, estimated channel, coded', 'OFDM, estimated channel, uncoded')
 xlabel('SNR')
 ylabel('BER')
-ylim([10^-5, 10^-0.5])
+ylim([10^-5, 10^-1])
 grid on
 title('BER for OFDM')
 
