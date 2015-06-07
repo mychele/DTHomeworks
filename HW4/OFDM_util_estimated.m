c21dc302028230187176e70762551f86076a9538
mychele <michele.polese@gmail.com>
2015-06-07 07:49:01 +0200

    New setup for blade - no parallel

--- HW4/OFDM_util_estimated.m
+++ HW4/OFDM_util_estimated.m
@@ -4,7 +4,7 @@ close all
 clc
 rng default
 
-parpool(8);
+%parpool(8);
 
 %% Data
 M = 512;
@@ -14,32 +14,32 @@ N2 = 4;
 desired_bits = 2^22;
 
 %% BER with coding, estimated channel
-repeat = 100;
+repeat = 50;
 isKnown = false;
 coding = true;
-snr_vec_coding_estimated = [0, 1, 2:0.05:2.6, 3];
-BER_coding_estimated = zeros(length(snr_vec_coding_estimated), repeat);
-parfor snr_i = 1:length(snr_vec_coding_estimated)
-    snr_c = snr_vec_coding_estimated(snr_i);
+snr_vec_coding_estimated_1 = 2:0.05:2.6;
+BER_coding_estimated_1 = zeros(length(snr_vec_coding_estimated_1), repeat);
+for snr_i = 1:length(snr_vec_coding_estimated_1)
+    snr_c = snr_vec_coding_estimated_1(snr_i);
     fprintf('Coded, snr = %.2f\n', snr_c);
     for i = 1:repeat
-        [BER_coding_estimated(snr_i, i), ~] = OFDM_BER(M, Npx, N2, t0, desired_bits, snr_c, coding, isKnown);
+        [BER_coding_estimated_1(snr_i, i), ~] = OFDM_BER(M, Npx, N2, t0, desired_bits, snr_c, coding, isKnown);
     end
 end
 
-save('OFDM_coded_estimated_long', 'BER_coding_estimated', 'snr_vec_coding_estimated', 'desired_bits');
+save('OFDM_coded_estimated_long_1', 'BER_coding_estimated_1', 'snr_vec_coding_estimated_1', 'desired_bits');
 %% BER without coding, estimated channel
-coding = false;
-snr_vec_nocoding_estimated = 0:15;
-BER_nocoding_estimated = zeros(length(snr_vec_nocoding_estimated), repeat);
-parfor snr_i = 1:length(snr_vec_nocoding_estimated)
-    snr_nc = snr_vec_nocoding_estimated(snr_i);
-    fprintf('Uncoded, snr = %.2f\n', snr_nc);
-    for i = 1:repeat 
-        [BER_nocoding_estimated(snr_i, i), ~] = OFDM_BER(M, Npx, N2, t0, desired_bits, snr_nc, coding, isKnown);
-    end
-end
-
-save('OFDM_uncoded_estimated_long', 'BER_nocoding_estimated', 'snr_vec_nocoding_estimated', 'desired_bits');
-
-delete(gcp);
+% coding = false;
+% snr_vec_nocoding_estimated = 0:15;
+% BER_nocoding_estimated = zeros(length(snr_vec_nocoding_estimated), repeat);
+% parfor snr_i = 1:length(snr_vec_nocoding_estimated)
+%     snr_nc = snr_vec_nocoding_estimated(snr_i);
+%     fprintf('Uncoded, snr = %.2f\n', snr_nc);
+%     for i = 1:repeat 
+%         [BER_nocoding_estimated(snr_i, i), ~] = OFDM_BER(M, Npx, N2, t0, desired_bits, snr_nc, coding, isKnown);
+%     end
+% end
+% 
+% save('OFDM_uncoded_estimated_long', 'BER_nocoding_estimated', 'snr_vec_nocoding_estimated', 'desired_bits');
+% 
+% delete(gcp);
--- HW4/OFDM_util_estimated_new.m
+++ HW4/OFDM_util_estimated_new.m
@@ -4,7 +4,7 @@ close all
 clc
 rng default
 
-parpool(8);
+%parpool(8);
 
 %% Data
 M = 512;
@@ -15,32 +15,32 @@ desired_bits = 2^22;
 estMethod = 2;
 
 %% BER with coding, estimated channel
-repeat = 100;
+repeat = 50;
 isKnown = false;
 coding = true;
-snr_vec_coding_estimated_2 = [0, 1, 2:0.05:2.6, 3];
-BER_coding_estimated_2 = zeros(length(snr_vec_coding_estimated_2), repeat);
-parfor snr_i = 1:length(snr_vec_coding_estimated_2)
-    snr_c = snr_vec_coding_estimated_2(snr_i);
+snr_vec_coding_estimated_4 = 2:0.05:2.6;
+BER_coding_estimated_4 = zeros(length(snr_vec_coding_estimated_4), repeat);
+for snr_i = 1:length(snr_vec_coding_estimated_4)
+    snr_c = snr_vec_coding_estimated_4(snr_i);
     fprintf('Coded, snr = %.2f\n', snr_c);
     for i = 1:repeat
-        [BER_coding_estimated_2(snr_i, i), ~] = OFDM_BER(M, Npx, N2, t0, desired_bits, snr_c, coding, isKnown, estMethod);
+        [BER_coding_estimated_4(snr_i, i), ~] = OFDM_BER(M, Npx, N2, t0, desired_bits, snr_c, coding, isKnown, estMethod);
     end
 end
 
-save('OFDM_coded_estimated_2_long', 'BER_coding_estimated_2', 'snr_vec_coding_estimated_2', 'desired_bits');
+save('OFDM_coded_estimated_2_long_2', 'BER_coding_estimated_4', 'snr_vec_coding_estimated_4', 'desired_bits');
 %% BER without coding, estimated channel
-coding = false;
-snr_vec_nocoding_estimated_2 = 0:15;
-BER_nocoding_estimated_2 = zeros(length(snr_vec_nocoding_estimated_2), repeat);
-parfor snr_i = 1:length(snr_vec_nocoding_estimated_2)
-    snr_nc = snr_vec_nocoding_estimated_2(snr_i);
-    fprintf('Uncoded, snr = %.2f\n', snr_nc);
-    for i = 1:repeat 
-        [BER_nocoding_estimated_2(snr_i, i), ~] = OFDM_BER(M, Npx, N2, t0, desired_bits, snr_nc, coding, isKnown, estMethod);
-    end
-end
+% coding = false;
+% snr_vec_nocoding_estimated_2 = 0:15;
+% BER_nocoding_estimated_2 = zeros(length(snr_vec_nocoding_estimated_2), repeat);
+% parfor snr_i = 1:length(snr_vec_nocoding_estimated_2)
+%     snr_nc = snr_vec_nocoding_estimated_2(snr_i);
+%     fprintf('Uncoded, snr = %.2f\n', snr_nc);
+%     for i = 1:repeat 
+%         [BER_nocoding_estimated_2(snr_i, i), ~] = OFDM_BER(M, Npx, N2, t0, desired_bits, snr_nc, coding, isKnown, estMethod);
+%     end
+% end
+% 
+% save('OFDM_uncoded_estimated_2_long', 'BER_nocoding_estimated_2', 'snr_vec_nocoding_estimated_2', 'desired_bits');
 
-save('OFDM_uncoded_estimated_2_long', 'BER_nocoding_estimated_2', 'snr_vec_nocoding_estimated_2', 'desired_bits');
-
-delete(gcp);
+%delete(gcp);
